8d5f024e850d5433facce3e6659e3ae0ee0316e2
Shawn McNaughton <shawngmc@gmail.com>
2023-10-29 01:06:16 -0400

    make executable

--- touchpad/repack-acpi.sh
+++ touchpad/repack-acpi.sh
@@ -3,5 +3,5 @@ cd /tmp/acpi
 
 mkdir -pv kernel/firmware/acpi
 cp dsdt.aml kernel/firmware/acpi
-find kernel | cpio -H newc --create > /boot/instumented_initrd
+find kernel | pio -H newc --create > /boot/instumented_initrd
 cat /boot/initrd >> /boot/instrumented_initrd
